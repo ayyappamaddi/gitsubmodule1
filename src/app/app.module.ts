1b436d023e571c1d4b9e16a2823dd35c488186ce
ayyappamaddi <ayyappa.maddi@valuelabs.com>
2021-03-23 19:04:52 +0530

    updating master component with commit and changes

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,13 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MicromoduleModule } from './micromodule/micromodule.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, MicromoduleModule
   ],
   providers: [],
   bootstrap: [AppComponent]
